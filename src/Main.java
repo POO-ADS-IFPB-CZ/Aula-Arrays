549e1d9c6d908e938fbb227c495a64a522bdad91
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-10-20 07:21:20 -0300

    Finalizando o exemplo dos jogos

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,25 @@ public class Main {
 
         if(jogos>0){
             for(int i=0; i<jogos;i++){
-                System.out.println("Informe a quantidade de números do jogo "+(i+1)+":");
+                //TODO: Verificar se a quantidade está entre 6 e 16
+                System.out.print("Informe a quantidade de números do jogo "+(i+1)+":");
                 int tamanho = scanner.nextInt();
                 matriz[i] = new int[tamanho];
+                for(int j=0; j<tamanho; j++){
+                    //TODO: Verificar se o número não é repetido e está entre 1 e 60
+                    System.out.print("Informe o número "+(j+1)+":");
+                    int numero = scanner.nextInt();
+                    matriz[i][j] = numero;
+                }
             }
+        }
+
+        System.out.println("Resumo dos jogos:");
+        for(int array[]: matriz){
+            for (int j: array){
+                System.out.print(j+" ");
+            }
+            System.out.println();
         }
 
     }
